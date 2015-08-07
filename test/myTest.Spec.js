3b3250cad750c41ee2b33d5bfbc5cf24e474ecfe
Ricardo Ibarra <coder10@gmail.com>
2015-08-06 22:53:36 -0700

    test suite

---
+++ test/myTest.Spec.js
@@ -0,0 +1,6 @@
+
+describe('Sample test', function() {
+	it('Initial dumb test', function() {	
+		expect(1).toBe(1);
+	});
+});
---
+++ test/second.Spec.js
@@ -0,0 +1,6 @@
+describe('Just another lazy test', function() {
+	it('Test 2', function() {
+		var x = 0;
+		expect(x).toBe(0);
+	});
+})
